c379341f2a5c6b5969f26335cd50064bcbf3840c
Andrej Toth <tandrej98@gmail.com>
2022-11-07 21:16:00 +0100

    feat: implement plane grpc api

--- plane/build.gradle.kts
+++ plane/build.gradle.kts
@@ -2,22 +2,35 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("application")
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm")
 }
 
 application {
     mainClass.set("sk.stuba.fei.asos.project24.plane.PlaneAppKt")
 }
 
-group = "sk.stuba.fei.asos.project24"
+group = rootProject.group
+val grpcVersion: String by rootProject.extra
+val grpcKotlinVersion: String by rootProject.extra
+val coroutinesVersion: String by rootProject.extra
+val logbackVersion: String by rootProject.extra
+val kotlinLogVersion: String by rootProject.extra
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
+    implementation(project(":plane-grpc-api"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("io.grpc:grpc-netty:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLogVersion")
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@
 rootProject.name = "asos-grpc-demo"
 include("plane")
 include("dispatching")
+include("plane-grpc-api")
