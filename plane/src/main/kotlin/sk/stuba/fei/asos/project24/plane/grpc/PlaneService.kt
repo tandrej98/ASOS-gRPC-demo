edaef4d7da18ff2c32b751fe60fcb3337713bc02
Andrej Toth <tandrej98@gmail.com>
2022-11-08 21:21:31 +0100

    feat: implement info call and prep for next development

--- plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/grpc/PlaneService.kt
+++ plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/grpc/PlaneService.kt
@@ -5,23 +5,25 @@ import kotlinx.coroutines.flow.flow
 import mu.KotlinLogging
 import sk.stuba.fei.asos.project24.plane.*
 import sk.stuba.fei.asos.project24.plane.config.Config
+import sk.stuba.fei.asos.project24.plane.data.PlaneData
+import sk.stuba.fei.asos.project24.plane.data.PlaneLocation
 import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration.Companion.seconds
 
 class PlaneService(
-    coroutineContext: CoroutineContext = Dispatchers.Default,
-    locations: List<Location>
+    coroutineContext: CoroutineContext = Dispatchers.Default
 ): PlaneGrpcKt.PlaneCoroutineImplBase(coroutineContext) {
     private val log = KotlinLogging.logger {}
-    private val locationIterator = locations.iterator()
+    private val locationIterator = PlaneLocation.locations.iterator()
     private lateinit var currentLocation: Location
 
     init {
-//        TODO: log plane ID
-        log.info("Starting plane service of plane {}", "TODO: ID")
-        if (locations.isEmpty()) throw IllegalArgumentException("Locations list is empty")
+        log.info("Starting plane service of plane ID: {}", PlaneData.id)
+        if (!locationIterator.hasNext()) throw IllegalArgumentException("Locations list is empty")
     }
 
+//    Location updates only while the dispatching is following the plane. Not Ideal
+//    but Ok considering the scope of this demo.
     override fun followLocation(request: LocationRequest) = flow {
         while (context.isActive) {
             if (locationIterator.hasNext()) {
@@ -37,7 +39,13 @@ class PlaneService(
     }
 
     override suspend fun information(request: PlaneInfoRequest): PlaneInfo {
-//        TODO: return real plane info object
-        return PlaneInfo.newBuilder().build()
+        return PlaneInfo.newBuilder()
+            .setId(PlaneData.id)
+            .setName(PlaneData.name)
+            .setType(PlaneData.type)
+            .setCapacity(PlaneData.capacity ?: 0)
+            .setStartCity(PlaneData.start)
+            .setEndCity(PlaneData.destination)
+            .build()
     }
 }
