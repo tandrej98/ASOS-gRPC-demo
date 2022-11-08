9df28cb99ff6af18b6464ed61299bf7dc33aa644
Andrej Toth <tandrej98@gmail.com>
2022-11-08 21:20:44 +0100

    feat: add env config

---
+++ plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/EnvKeys.kt
@@ -0,0 +1,12 @@
+package sk.stuba.fei.asos.project24.plane
+
+object EnvKeys {
+    const val CONFIG_PORT_KEY = "GRPC_PORT"
+    const val CONFIG_DELAY_KEY = "DELAY_SECONDS"
+    const val PLANE_ID_KEY = "PLANE_ID"
+    const val PLANE_TYPE_KEY = "PLANE_TYPE"
+    const val PLANE_NAME_KEY = "PLANE_NAME"
+    const val PLANE_CAP_KEY = "PLANE_CAPACITY"
+    const val PLANE_START_KEY = "PLANE_START"
+    const val PLANE_DEST_KEY = "PLANE_DESTINATION"
+}
--- plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/config/Config.kt
+++ plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/config/Config.kt
@@ -1,6 +1,8 @@
 package sk.stuba.fei.asos.project24.plane.config
 
+import sk.stuba.fei.asos.project24.plane.EnvKeys
+
 object Config {
-    val grpcPort: Int = System.getenv("GRPC_PORT")?.toInt() ?: 8080
-    val delaySeconds: Long = System.getenv("DELAY_SECONDS")?.toLong() ?: 5L
+    val grpcPort: Int = System.getenv(EnvKeys.CONFIG_PORT_KEY)?.toInt() ?: 8080
+    val delaySeconds: Long = System.getenv(EnvKeys.CONFIG_DELAY_KEY)?.toLong() ?: 5L
 }
