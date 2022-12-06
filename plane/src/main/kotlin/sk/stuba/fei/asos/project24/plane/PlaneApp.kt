025234efdf166ee1cf191fc7ac15a3481c0b3702
Andrej Toth <tandrej98@gmail.com>
2022-12-06 18:27:43 +0100

    feat: integrate location generation by p.sobota

--- plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/PlaneApp.kt
+++ plane/src/main/kotlin/sk/stuba/fei/asos/project24/plane/PlaneApp.kt
@@ -1,8 +1,10 @@
 package sk.stuba.fei.asos.project24.plane
 
+import sk.stuba.fei.asos.project24.plane.data.PlaneLocation
 import sk.stuba.fei.asos.project24.plane.grpc.Server
 
 fun main() {
+    PlaneLocation //Trigger the init block
     val grpcServer = Server()
     grpcServer.start()
 }
